b5ba670dab98ab7355f194b7055b4ddf9823aad6
hackgods <saurabhsuresh4s@gmail.com>
2024-03-09 23:52:54 +0000

    SEO optimization added

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Saurabh Suresh's Portfolio",
   description: "Saurabh is a Full Stack Software Engineer with experience in Web, Mobile, and Software development based out of Newcastle upon Tyne, United Kingdom.",
+  openGraph: {
+    title: "Saurabh Suresh's Portfolio",
+    description: "Saurabh is a Full Stack Software Engineer with experience in Web, Mobile, and Software development based out of Newcastle upon Tyne, United Kingdom.",
+    url: "https://www.saurabhsuresh.com",
+    siteName: "Saurabh Suresh's Portfolio"
+  },
+  keywords: ['Saurabh Suresh', 'Full stack developer', 'Newcastle University student', 'Newcastle software developer'],
+  twitter:{
+    card: "summary_large_image",
+    title: "Saurabh Suresh's Portfolio",
+    description: "Saurabh is a Full Stack Software Engineer with experience in Web, Mobile, and Software development based out of Newcastle upon Tyne, United Kingdom.",
+    creator: "@hxckgxd",
+    images: ['https://drive.usercontent.google.com/download?id=1GmmtkM4Q1LJqyqgjtkW7FUrZankr_rwa']
+  }
 };
 
 export default function RootLayout({
