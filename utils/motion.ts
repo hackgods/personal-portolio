55c7bf17d421fbd5035fe8863225859ccebaa34a
hackgods <saurabhsuresh4s@gmail.com>
2024-03-10 02:02:23 +0000

    fadeUp motion delay remove

--- utils/motion.ts
+++ utils/motion.ts
@@ -44,7 +44,6 @@ export function slideInFromLeft(delay: number) {
       opacity: 1,
       scale: 1,
       transition: {
-        delay: 0.1,
         duration: 0.5,
         ease: [0.075, 0.82, 0.165, 1],
       },
